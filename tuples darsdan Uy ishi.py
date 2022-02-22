40eebc6e1574b720db8d1c9aad6a37f85c8ffb9f
Abduboriy007 <100182495+Abduboriy007@users.noreply.github.com>
2022-02-22 10:00:40 +0500

    Add files via upload

---
+++ tuples darsdan Uy ishi.py
@@ -0,0 +1,13 @@
+#1
+# list1 = [100, 200, 300, 400, 500]
+# list2=list1[::-1]
+# print(list2)
+
+#2
+list1 = ["M", "na", "i", "jo"]
+list2 = ["y", "me", "s", "hn"]
+
+c=[]
+
+list3=list1[0]+list2[0],list1[1]+list2[1],list1[2]+list2[2],list1[3]+list2[3]
+print(list3) 
